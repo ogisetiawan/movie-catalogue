d162bcc52cb5297fc0aa381af72f7a6c29b235fa
ogisetiawan <ogisetiawan21@gmail.com>
2023-03-13 16:33:32 +0700

    feat: add api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: 'a2df3d1a7611194432bbdf1fc80540f2',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
